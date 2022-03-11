434496379cb25c54d20461baa42eca249aa445e6
Ray <89287773+RayNanta@users.noreply.github.com>
2022-03-11 15:39:14 +0700

    Add files via upload

---
+++ src/pages/Products/Data.js
@@ -0,0 +1,63 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you achive your business goal!',
+    headline: 'Lead Generation Specialiast for Online Businesses',
+    lightText: 'true',
+    topline:'Marketing Agency',
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Image',
+    start: ''
+}
+
+export const homeObjTwo = {
+    primary: false,
+    lightBg: true,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightTextDesc: false,
+    buttonLabel: 'See Products',
+    description: 'Your business growth is our main commitment. Our clients are spreading all over the world. Click to see more about out products!',
+    headline: 'Our Priority and Commitment for You!',
+    lightText: false,
+    topline:'Our Commitment',
+    img: require('../../images/profile.jpg'),
+    alt: 'Image',
+    start: true
+}
+
+export const homeObjThree = {
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'All of your business risk and problems are going to be much easier to handle by our professional analyics method.',
+    headline: 'Fully Accurate and Comprehensive Analytics ',
+    lightText: 'true',
+    topline:'Our Professionalism',
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Image',
+    start: ''
+}
+
+export const homeObjFour = {
+    primary: true,
+    lightBg: false,
+    imgStart: 'start',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We offer your business to become somtehing more. Do not hesitate to call our customer services for an advance help!',
+    headline: 'Our Team is Your Business Solution',
+    lightText: 'true',
+    topline:'Your Main Solution',
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Image',
+    start: true
+}
